2d0d1f27ace30f9a1a3be5c14f4e9d13790870b1
raju-islam <rajuslam39@gmail.com>
2024-01-08 15:03:35 +0600

    Initial commit first commit for Getx

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'package:getx/page/home_page.dart';
+
+void main(){
+  runApp(MyApp());
+
+
+}
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
